8f10771eecadc6fd68eb342bc6e966acecdb053f
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-06-07 15:28:50 +0200

    add LogsAdapter
    Much like CatsAdapter, the new class is similarly equipped with move(),
    delete(), add() methods as well as it's tracking whether any of those
    was used in order to know if data was edited and needs saving.

--- app/src/main/java/com/example/sokol/monitor/LogsDialog/LogsAdapter.java
+++ app/src/main/java/com/example/sokol/monitor/LogsDialog/LogsAdapter.java
@@ -1,4 +1,84 @@
 package com.example.sokol.monitor.LogsDialog;
 
-public class LogsAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.sokol.monitor.R;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LogsAdapter extends RecyclerView.Adapter<LogsAdapter.ViewHolder> {
+
+    List<Log> mItems;
+    private boolean mWasDataChanged = false;
+
+    public boolean isDataChanged() {
+        return mWasDataChanged;
+    }
+
+    public LogsAdapter(List<Log> data_to_show){
+        mItems = data_to_show;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.logs_dialog_recycler_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public Log getLogAt(int pos){
+        return mItems.get(pos);
+    }
+
+    public void move(int fromPos, int toPos){
+        if (fromPos < toPos) {
+            for (int i = fromPos; i < toPos; i++) {
+                Collections.swap(mItems, i, i + 1);
+            }
+        } else {
+            for (int i = fromPos; i > toPos; i--) {
+                Collections.swap(mItems, i, i - 1);
+            }
+        }
+        notifyItemMoved(fromPos, toPos);
+        mWasDataChanged = true;
+    }
+
+    public void remove(int pos){
+        mItems.remove(pos);
+        notifyItemRemoved(pos);
+        mWasDataChanged = true;
+    }
+
+    public void addACat(Log log) {
+        mItems.add(log);
+        notifyItemInserted(mItems.size() - 1);
+        mWasDataChanged = true;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView initialView;
+        public TextView durationview;
+        public TextView startTimeView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            initialView = itemView.findViewById(R.id.initial);
+            durationview = itemView.findViewById(R.id.duration);
+            startTimeView = itemView.findViewById(R.id.start_time);
+        }
+    }
 }
