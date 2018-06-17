a1c3baf48e474a4842e095793acfe6c4d687bbf5
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-06-17 14:43:19 +0200

    fix PeriodicDistro's unit test
    Updated unit test to work with current version of the class. Renamed
    file and removed example code as unrelated to PeriodicDistro class.

--- app/src/test/java/com/example/sokol/monitor/PeriodicDistroTests.java
+++ app/src/test/java/com/example/sokol/monitor/PeriodicDistroTests.java
@@ -4,16 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
+public class PeriodicDistroTests {
 
     @Test
     public void periodicDistro_isDailyOK() throws Exception{
@@ -23,6 +14,7 @@ public class ExampleUnitTest {
 
         long[] starts = new long[]{0, day-50, day-100, day, day2+10, day3-50};
         long[] ends   = new long[]{0, day+50, day, day+100, day2+110, day3+80};
+        long[] catIDs   = new long[]{0, 0, 0, 0, 0, 0};
 
         long zeroDay = TimeHelper.get0HourNdaysAgo(0)-day*3;
         for(int i=0; i< starts.length;i++){
@@ -31,7 +23,7 @@ public class ExampleUnitTest {
         }
 
         long[] desired_output = new long[]{150, 150, 150};
-        LogsData logs = new LogsData(starts, ends, ends);
+        LogsData logs = new LogsData(starts, ends, ends, catIDs);
         PeriodicDistro peri = new PeriodicDistro(logs);
 
         assertArrayEquals(desired_output, peri.mDaily);
