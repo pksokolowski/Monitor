794f10de7f73102c0aec1f62bef15cc246b0317d
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-06-28 14:25:01 +0200

    improve easy logs ui

--- app/src/main/java/com/example/sokol/monitor/EasyLogsDialog/EasyUILogsFragment.java
+++ app/src/main/java/com/example/sokol/monitor/EasyLogsDialog/EasyUILogsFragment.java
@@ -1,27 +1,40 @@
 package com.example.sokol.monitor.EasyLogsDialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowInsets;
 
 import com.example.sokol.monitor.R;
 import com.example.sokol.monitor.SingleInstanceDialog;
 
-public class EasyUILogsFragment extends DialogFragment{
+public class EasyUILogsFragment extends DialogFragment {
     private View mView;
 
-    @Nullable
+
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
         mView = inflater.inflate(R.layout.easy_ui_dialog, null);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(mView);
 
-        return mView;
+        return builder.create();
     }
+
 }
